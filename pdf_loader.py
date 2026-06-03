940faf8e7a16fbae8a63fa639704d2d86b636d63
rohini-ray <rohini.raysub@gmail.com>
2026-06-03 21:19:28 +0530

    update

--- pdf_loader.py
+++ pdf_loader.py
@@ -1,12 +1,15 @@
-import fitz
+from pypdf import PdfReader
 
 def extract_text(pdf_path):
 
-    doc = fitz.open(pdf_path)
+    reader = PdfReader(pdf_path)
 
     text = ""
 
-    for page in doc:
-        text += page.get_text()
+    for page in reader.pages:
+        page_text = page.extract_text()
+
+        if page_text:
+            text += page_text
 
     return text
